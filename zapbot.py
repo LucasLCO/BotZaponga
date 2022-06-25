d82feea27e97626e9e8efd45b7b4817ec66090d5
Lucas Oliveira <lucaslco2005@gmail.com>
2022-06-25 14:54:01 -0300

    Update zapbot.py

--- zapbot.py
+++ zapbot.py
@@ -3,23 +3,27 @@ import time
 
 mensagens = list()
 sim = True
-while sim:
-    mensagens.append(str(input("Digite a mensagem: ")).strip())
-    resp = str(input("Deseja adicionar mais alguma mensagem? [S/N]").strip().upper())
-    if resp not in 'NSsn':
-        print("Erro... Digite novamente")
-    elif resp in 'N':
-        sim = False
-    else:
-        sim = True
+
+def BotZap(modo):
+    if modo == 'M':
+        while sim:
+            mensagens.append(str(input("Digite a mensagem: ")).strip())
+            resp = str(input("Deseja adicionar mais alguma mensagem? [S/N]").strip().upper())
+            if resp not in 'NSsn':
+                print("Erro... Digite novamente")
+            elif resp in 'N':
+                sim = False
+            else:
+                sim = True
+        time.sleep(20)
+        while True:
+            for texto in mensagens:
+                pyautogui.typewrite(texto)
+                pyautogui.press("enter")
+    elif modo == 'F':
+        time.sleep(20)
+        while True:
+            pyautogui.click()
 
 
-def BotZap(mensagem):
-    time.sleep(20)
-    while True:
-        for texto in mensagem:
-            pyautogui.typewrite(texto)
-            pyautogui.press("enter")
-
-
-BotZap(mensagens)
+BotZap('F')
